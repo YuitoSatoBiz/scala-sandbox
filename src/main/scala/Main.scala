3202d58e88e2f118bb4561634b1c6b024e542bb1
YuitoSatoBiz <yuito.sato@bizreach.co.jp>
2017-09-15 01:39:59 +0900

    fix

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,9 @@
-import scala.collection.mutable.ListBuffer
-
 /**
   * Created by satoyuito on 2017/06/19.
   */
 object Main {
 
   def main(args: Array[String]): Unit = {
-<<<<<<< HEAD
     SortPractice.test()
-=======
-    val list1 = MyList(1, MyList(2, MyNil))
-    val list2 = MyList(4, MyNil)
-    val list3 = 3::list2
-    val list4 = (list1 ::: list3).map { e =>
-      e + 1
-    }
-    list4.foreach { e =>
-      println(e)
-    }
-
-    ListBuffer
->>>>>>> 946aeafda1ace0cf4e0a7fca5137f49e01d842bf
   }
 }
